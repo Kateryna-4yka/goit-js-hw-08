c6808a5acb9c354ab422c0cbe21b2f42161e50a0
Kateryna-4yka <kotasyri@gmail.com>
2024-12-20 21:31:52 +0200

    1

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelector("#name-input"); 
-
-const span = document.querySelector("#name-output")
-
-input.addEventListener(`input`, ()=> {
-    span.textContent = input.value.trim().length > 0 ? input.value.trim() : "Anonymous";
-});
